81cf1e8fdf745ec8645f03b2196b8e795edc0e72
XDEric <eeeric3995@yahoo.com>
2020-10-21 23:44:30 -0400

    simple function

--- Practice.cpp
+++ Practice.cpp
@@ -5,7 +5,32 @@ using namespace std;
 //Global variables
 int myNum = 20;
 
+void Tester(int x, int y){
+    for (int i = 0; i < 5; i++)
+    {
+        cout<< "\nTest:"<<i + 1;
+        if (i < x) {
+        cout << "\nsum:" <<(x+i);
+        }
+        else
+        {
+            cout << "\nsum:" <<(i+y);
+        }
+        
+    }
+}
+
 int main()
 {
+    int num1 = 1;
+    int num2 = 2;
+    int num3 = 3, num4 = 4;
     cout <<"Test"<< myNum;
+    //cout << "\nsum:" <<(num1+num4);
+    Tester(num3, num4);
+    
 }
+
+    
+
+
